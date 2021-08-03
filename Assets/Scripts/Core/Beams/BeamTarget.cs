be763a79b2b44a0df36663e253911f6f636d506b
Bowers-L <loganmbowers@gmail.com>
2021-08-03 18:44:29 -0400

    Removed default skybox and added new lighting

--- Assets/Scripts/Core/Beams/BeamTarget.cs
+++ Assets/Scripts/Core/Beams/BeamTarget.cs
@@ -23,17 +23,33 @@ namespace Beam.Core.Beams
             }
         }
 
+        public void FixedUpdate()
+        {
+            //Beam is attached to something
+            if (currSource != null)
+            {
+                //TEMPORARY SOLUTION: Need to handle collisions & physics.
+                Vector3 targetPos = currSource.transform.position + currSource.transform.forward * currBeamDist;
+                rb.MovePosition(targetPos);
+
+                //Stop translational and rotational velocity
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                transform.forward = currSource.transform.forward;
+            }
+        }
+
         public void attachBeam(BeamSource source, Ray beam)
         {
             currSource = source;
-            EventManager.StartListening<BeamSourceMoved, BeamSource>(handleBeamMoved);
+            //EventManager.StartListening<BeamSourceMoved, BeamSource>(handleBeamMoved);
             EventManager.StartListening<BeamRelease, BeamSource>(detachBeam);
 
-            //snap object to the cursor focus
-
+            //snap object center to the cursor
             rb.MovePosition(UnityEngineExt.projectPointOntoLine(transform.position, beam));
             currBeamDist = (transform.position - source.transform.position).magnitude;
 
+            rb.useGravity = false;
 
             //Change sprite to highlight change.
         }
@@ -43,18 +59,23 @@ namespace Beam.Core.Beams
             if (currSource == source)
             {
                 currSource = null;
-                EventManager.StopListening<BeamSourceMoved, BeamSource>(handleBeamMoved);
+                //EventManager.StopListening<BeamSourceMoved, BeamSource>(handleBeamMoved);
                 EventManager.StopListening<BeamRelease, BeamSource>(detachBeam);
+
+                rb.useGravity = true;
             }
         }
 
         //The Callee is responsible for checking if the source is the same as currSource!
+        /*
         public void handleBeamMoved(BeamSource source)
         {
             if (currSource == source)
             {
-                rb.MovePosition(source.transform.position + source.transform.forward * currBeamDist);
+                Vector3 targetPos = source.transform.position + source.transform.forward * currBeamDist;
+                rb.MovePosition(targetPos);
             }
         }
+        */
     }
 }
