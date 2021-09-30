5fab31932a762210504e626672dd9c1d366aeab6
ChaseOB <chaseobrien128@gmail.com>
2021-09-30 19:51:52 -0400

    MP fixes

--- Assets/Scripts/Triggers/ProximityTrigger.cs
+++ Assets/Scripts/Triggers/ProximityTrigger.cs
@@ -8,14 +8,7 @@ namespace Beam.Triggers
     public class ProximityTrigger : Trigger
     {
         public List<string> tags; //List of all tags which trigger the sensor
-        void Start()
-        {
-        
-        }
-        void Update()
-        {
-        
-        }
+      
         public void OnTriggerEnter(Collider other)
         {
             foreach(string tag in tags)
