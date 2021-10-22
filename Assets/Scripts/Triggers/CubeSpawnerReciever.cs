8b1b20e4014981b9f4c18dde3bfd25f6081a9b62
Bowers-L <loganmbowers@gmail.com>
2021-10-22 16:09:08 -0400

    Made basic cube spawner prefab

--- Assets/Scripts/Triggers/CubeSpawnerReciever.cs
+++ Assets/Scripts/Triggers/CubeSpawnerReciever.cs
@@ -6,46 +6,45 @@ namespace Beam.Triggers
 {
     public class CubeSpawnerReciever : TriggerReceiver
     {
-        public float bufferTime;
-        float bufferTimer;
-        public int maxCubes;
-        int cubeCount;
         public GameObject cubePrefab;
+        public float bufferTime;
+        public bool infiniteCubes;
+        public int maxCubes;
 
-
+        //float bufferTimer;
+        int cubeCount;
 
         // Start is called before the first frame update
         new public void Start()
         {
             base.Start();
             cubeCount = 0;
-            bufferTimer = 0;
-            countdown();
+            //bufferTimer = 0;
         }
 
         public override void HandleActivated()
         {
-            if (cubeCount < maxCubes && bufferTimer == bufferTime)
-            {
-                bufferTimer = 0f;
-                GameObject newCube = Instantiate(cubePrefab, gameObject.transform.position, cubePrefab.transform.rotation);
-                cubeCount++;
-            }
+            StartCoroutine(countdown());
         }
 
         public override void HandleDeactivated()
         {
-            countdown();
+            //Don't do anything because cube spawners only respond to being activated.
         }
 
-
-        void countdown()
+        IEnumerator countdown()
         {
-            while (!(bufferTimer == bufferTime))
+            yield return new WaitForSeconds(bufferTime);
+
+            if (infiniteCubes || cubeCount < maxCubes)
             {
-                bufferTimer += Time.deltaTime;
+                //bufferTimer = 0f;
+                GameObject newCube = Instantiate(cubePrefab, gameObject.transform.position, cubePrefab.transform.rotation);
+                listenee = newCube.GetComponent<PlaneTrigger>(); //Change the listenee to be the cube that just spawned
+                cubeCount++;
             }
 
+            yield return null;
         }
     }
 }
---
+++ Assets/Scripts/Triggers/PlaneTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Beam.Triggers
+{
+    class PlaneTrigger : Trigger
+    {
+        /*
+        /* Def. of plane:
+         * d - How far from the origin the plane is (in direction of normal vector, can be negative)
+         * theta - Angle from x axis to the vector on the xz plane (CCW)
+         * phi - -PI/2 to PI/2 starting at the xz plane and going towards the y-axis.
+         * theta and phi together determine the normal vector.
+         * EX: The xz plane is (0, 0, PI/2), zy plane is (0, 0, 0), xy plane is (0, PI/2, 0)
+         * Yup, not doing this rn.
+        public Vector3 plane;
+        */
+
+        public float killPlaneY;
+
+        public void Update()
+        {
+            if (transform.position.y < killPlaneY)
+            {
+                Debug.Log("Destroyed");
+                activate();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
