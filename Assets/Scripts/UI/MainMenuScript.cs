95a817bff05c61738ac5884cabb02662a90ec21c
ChaseOB <chaseobrien128@gmail.com>
2021-11-09 21:37:41 -0500

    Main Menu Foundations

---
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame(string log)
+    {
+            Debug.Log(log);
+    }
+
+
+}
