cb447897e3208eb0a8d8671aec80256d496d8ab2
ChaseOB <chaseobrien128@gmail.com>
2021-12-01 00:18:53 -0500

    fixed missing ceiling in glass tower

--- Assets/Scripts/Triggers/NarrationAudioReceiver.cs
+++ Assets/Scripts/Triggers/NarrationAudioReceiver.cs
@@ -8,14 +8,18 @@ namespace Beam.Triggers
     public class NarrationAudioReceiver : MonoBehaviour
     {
         public AudioSource audioSource;
-        //public AudioClip[] clips;
         public Dictionary<Trigger, AudioClip> clipDictionary = new Dictionary<Trigger, AudioClip>();
         public List<Trigger> triggers;
         public List<AudioClip> clips;
+        public List<Trigger> activated = new List<Trigger>();
 
         public Queue<AudioClip> clipQueue = new Queue<AudioClip>();
         //public AudioClip clip;
 
+        private void Awake() 
+        {
+            activated.Clear();
+        }
         private void Start() 
         {
             EventManager.StartListening<TriggerActivatedEvent, Trigger>(OnActivated);
@@ -23,7 +27,7 @@ namespace Beam.Triggers
             {
                 Debug.LogWarning("You must have the same number of triggers and audio clips");
                 return;
-            }    
+            }
             for(int i = 0; i < clips.Count; i++)
             {
                 clipDictionary.Add(triggers[i], clips[i]);
@@ -44,10 +48,10 @@ namespace Beam.Triggers
         public void OnActivated(Trigger trigger)
         {
             Debug.Log(trigger);
-            if (triggers.Contains(trigger))
+            if (triggers.Contains(trigger) && !activated.Contains(trigger))
             {
                 clipQueue.Enqueue(clipDictionary[trigger]);
-                triggers.Remove(trigger); //prevents the audio from being played multiple times
+                activated.Add(trigger); 
             }
         }
 
